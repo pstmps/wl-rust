cefb787d1c67453f38574efd9702dbf14de1a1c8
mps <m.stiebing@gmail.com>
2024-11-10 14:54:09 +0100

    removed unwrap() from main code

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 use petgraph::graph::{DiGraph, NodeIndex};
-use petgraph::Direction;
-use blake2::{Blake2b, Blake2bVar, Blake2s256, Digest};
+use blake2::Blake2bVar;
 use blake2::digest::{Update, VariableOutput};
-use std::collections::{HashMap, HashSet};
-use std::result;
+use std::collections::HashMap;
+
 use hex;
 use std::error::Error;
 
@@ -19,34 +18,44 @@ fn hash_label(label: &str, digest_size: usize) -> Result<String, Box<dyn Error>>
 fn init_node_labels(graph: &DiGraph<String, ()>, node_attr: Option<&str>) -> HashMap<NodeIndex, String> {
     graph.node_indices().map(|node| {
         let label = match node_attr {
-            Some(attr) => graph[node].clone(),
+            Some(_attr) => graph[node].clone(),
             None => graph.neighbors_undirected(node).count().to_string(),
         };
         (node, label)
     }).collect()
 }
 
-fn neighborhood_aggregate(graph: &DiGraph<String, ()>, node: NodeIndex, node_labels: &HashMap<NodeIndex, String>) -> String {
+fn neighborhood_aggregate(graph: &DiGraph<String, ()>, node: NodeIndex, node_labels: &HashMap<NodeIndex, String>) -> Result<String, Box<dyn Error>> {
+    let error_message = "{} label for {:?} not found";
+
     let mut label_list: Vec<String> = graph.neighbors(node)
-        .map(|nbr| node_labels.get(&nbr).unwrap().clone())
-        .collect();
+        .map(|neighbor| -> Result<String, Box<dyn Error>> {
+            node_labels.get(&neighbor)
+                .ok_or_else(|| format!("{}{}{:?}", error_message, "Neighbor", neighbor).into())
+                .map(|label| label.clone())
+        })
+        .collect::<Result<Vec<String>, Box<dyn Error>>>()?;
+
     label_list.sort();
-    node_labels.get(&node).unwrap().clone() + &label_list.concat()
+    let node_label = node_labels.get(&node)
+        .ok_or_else(|| -> String { format!("{}{}{:?}", error_message, "Node", node).into()} )?;
+
+    Ok(node_label.clone() + &label_list.concat())
 }
 
-fn weisfeiler_lehman_step(graph: &DiGraph<String, ()>, labels: &HashMap<NodeIndex, String>, digest_size: usize) -> HashMap<NodeIndex, String> {
+fn weisfeiler_lehman_step(graph: &DiGraph<String, ()>, labels: &HashMap<NodeIndex, String>, digest_size: usize) -> Result<HashMap<NodeIndex, String>, Box<dyn Error>> {
     graph.node_indices().map(|node| {
-        let label = neighborhood_aggregate(graph, node, labels);
-        (node, hash_label(&label, digest_size).unwrap())
-    }).collect()
+        let label = neighborhood_aggregate(graph, node, labels)?;
+        hash_label(&label, digest_size).map(|hashed_label| (node, hashed_label))
+    }).collect::<Result<HashMap<_, _>, _>>()
 }
 
-pub fn weisfeiler_lehman_graph_hash(graph: &DiGraph<String, ()>, node_attr: Option<&str>, iterations: usize, digest_size: usize) -> String {
+pub fn weisfeiler_lehman_graph_hash(graph: &DiGraph<String, ()>, node_attr: Option<&str>, iterations: usize, digest_size: usize) -> Result<String, Box<dyn Error>> {
     let mut node_labels = init_node_labels(graph, node_attr);
 
     let mut subgraph_hash_counts = Vec::new();
     for _ in 0..iterations {
-        node_labels = weisfeiler_lehman_step(graph, &node_labels, digest_size);
+        node_labels = weisfeiler_lehman_step(graph, &node_labels, digest_size)?;
         let mut counter = HashMap::new();
         for label in node_labels.values() {
             *counter.entry(label.clone()).or_insert(0) += 1;
@@ -57,7 +66,7 @@ pub fn weisfeiler_lehman_graph_hash(graph: &DiGraph<String, ()>, node_attr: Opti
     }
 
     let final_label = format!("{:?}", subgraph_hash_counts);
-    hash_label(&final_label, digest_size).unwrap()
+    hash_label(&final_label, digest_size)
 }
 
 #[cfg(test)]
@@ -85,7 +94,7 @@ mod tests {
         graph.add_edge(a, b, ());
         graph.add_edge(b, c, ());
 
-        let hash = weisfeiler_lehman_graph_hash(&graph, None, 3, 16);
+        let hash = weisfeiler_lehman_graph_hash(&graph, None, 3, 16).unwrap();
         println!("{}",hash);
         assert!(!hash.is_empty());
     }
@@ -114,8 +123,8 @@ mod tests {
         g2.add_edge(n7, n5, ());
         g2.add_edge(n7, n8, ());
 
-        let hash1 = weisfeiler_lehman_graph_hash(&g1, None, 3, 16);
-        let hash2 = weisfeiler_lehman_graph_hash(&g2, None, 3, 16);
+        let hash1 = weisfeiler_lehman_graph_hash(&g1, None, 3, 16).unwrap();
+        let hash2 = weisfeiler_lehman_graph_hash(&g2, None, 3, 16).unwrap();
 
         println!("Isomorphic");
         println!("Hash1: {}", hash1);
@@ -148,8 +157,8 @@ mod tests {
         g2.add_edge(n7, n5, ());
         g2.add_edge(n7, n8, ());
 
-        let hash1 = weisfeiler_lehman_graph_hash(&g1, Some("label"), 3, 16);
-        let hash2 = weisfeiler_lehman_graph_hash(&g2, Some("label"), 3, 16);
+        let hash1 = weisfeiler_lehman_graph_hash(&g1, Some("label"), 3, 16).unwrap();
+        let hash2 = weisfeiler_lehman_graph_hash(&g2, Some("label"), 3, 16).unwrap();
 
         println!("Non-Isomorphic");
         println!("Hash1: {}", hash1);
